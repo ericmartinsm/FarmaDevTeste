70402fbac85466dc625ad2c967d435e374e49f02
Eric Martins <ericmartins1997@gmail.com>
2021-01-31 22:07:53 -0300

    Front quase acabado

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,39 @@
 <template>
-  <div>
-      <input type="text" name="" id="" v-model="Personagem_nome">
-      <input type="button" value=" Search" v-on:click="get_api">
-      <h2>Nome Personagem: {{Personagem_nome}}</h2>
+  <div class="container-cards">
+      <div id="logo">
+        <img  src="../assets/logo.png" alt="">
+      </div>
+      <input >
+      <v-text-field v-model="Personagem_nome" label="Digite o Nome do Personagem"  hide-details="auto"></v-text-field>
+      <v-btn id="button-search" type="button" value=" Search" v-on:click="get_api" block>
+      Buscar
+      </v-btn>
+      
+      <div class="v-item-group ">
 
-      <div v-for="(id, status) in Personagem" :key="status">
-          <h3>Nome{{id.name}}</h3>
-          <p>Estado{{id.status}}</p>
-          <p>Espécie{{id.species}}</p>
-          <p>Genero{{id.gender}}</p>
-          <p>Primeira aparição{{id.origin[0]}}</p>
-          <p>Ultima aparição{{id.location[0]}}</p>
-          <img :src="id.image" alt="">
-          <p>Quantidade de Episódios{{id.episode}}</p>
+        <div id="card-place" class="v-card" v-for="(id, status) in Personagem" :key="status">
+            <v-expansion-panels>
+            <v-expansion-panel >
+                      <v-expansion-panel-header>
+                        <h3>{{id.name}}</h3> <v-avatar><img :src="id.image" alt=""></v-avatar>
+                      </v-expansion-panel-header>
+                      <v-expansion-panel-content> 
+
+                        <div class="card">
+                        <h1>{{id.name}}</h1>
+                        <img :src="id.image" alt="">   
+                        <p>Status</p> {{id.status}}
+                        <p>Specie</p> {{id.species}}
+                        <p>Gender</p> {{id.gender}}
+                        <p>First Apparition</p>  {{id.origin[0]}}
+                        <p>Last apparition</p>  {{id.location[0]}}                     
+                        <p>Number of Episodes Participated</p> {{id.episode.length}}
+                        </div> 
+                    </v-expansion-panel-content>
+            </v-expansion-panel>
+            </v-expansion-panels>      
+        </div>
+      
       </div>
   </div>
 </template>
@@ -33,13 +54,10 @@ export default {
 
         let url_padrao="https://rickandmortyapi.com/api/character/?name=";
 
-        console.log(url_padrao)
-        console.log(this.Personagem_nome)
-
         axios.get(url_padrao+this.Personagem_nome).then(res=>{
-          console.log(res);
+          
           this.Personagem=res.data.results;
-          console.log(res.data.results)
+          
         })
         
       }
@@ -48,9 +66,44 @@ export default {
 </script>
 
 <style>
-  div{
-    background:rgba(255, 253, 253, 0.103);
-    color: #fff;
+
+@import url('https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Suez+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Suez+One&display=swap');
+  .container-cards{
+    background:#A967DB;
+    color: rgb(0, 0, 0);
+    margin-top: -5px;
   }
+  #button-search{
+    font-family: 'Suez One', serif;
+    letter-spacing: 10px;
+    font-size: 20px;
+  }
+  #card-place h3{
+    font-family: 'Suez One', serif;
+  }
+  .card{
+    margin-top: 50px;
+    text-align: center; 
+  }
+  .card p{
+    font-family: 'Suez One', serif;
+    background:#5007F6;
+    border-radius: 150px;
+  }
+  .card img{
+    border-radius: 150px;
+  }
+  .card h1{
+    font-family: 'Gloria Hallelujah', cursive;
+  }
+  .v-item-group{
+    margin-top: 9px;
+  }
+  #logo{
+    text-align: center;
+  }
+  
   
 </style>
